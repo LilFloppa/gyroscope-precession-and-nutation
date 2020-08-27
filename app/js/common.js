5aa7e1bcba6a4630b0201aaa2bbae1326a4f8f91
IgorMuramshchikov <igor.muram@gmail.com>
2020-08-27 18:28:19 +0300

    Fix timer

--- app/js/common.js
+++ app/js/common.js
@@ -42,42 +42,42 @@ $(function () {
 	setText();
 
 	/* Timer */
-	let min = 0,
-	sec = 0,
-	ms = 0;
-	let isStoped = false,
-	isStarted = false;
+	// let min = 0,
+	// sec = 0,
+	// ms = 0;
+	// let isStoped = false,
+	// isStarted = false;
 
-	function Timer() {
-		setInterval(function Time() {
-			if (!isStoped) {
-				sec++;
-				if (sec >= 60) {
-					min++;
-					sec -= 60;
-				}
+	// function Timer() {
+	// 	setInterval(function Time() {
+	// 		if (!isStoped) {
+	// 			sec++;
+	// 			if (sec >= 60) {
+	// 				min++;
+	// 				sec -= 60;
+	// 			}
 
-				let minute = min < 10 ? "0" + min : min,
-				second = sec < 10 ? "0" + sec : sec;
+	// 			let minute = min < 10 ? "0" + min : min,
+	// 			second = sec < 10 ? "0" + sec : sec;
 
-				$(".timer__minutes").html(minute);
-				$(".timer__seconds").html(second);
-			}
-		}, 1000);
+	// 			$(".timer__minutes").html(minute);
+	// 			$(".timer__seconds").html(second);
+	// 		}
+	// 	}, 1000);
 
-		setInterval(function Milliseconds() {
-			if (!isStoped) {
-				ms++;
-				if (ms >= 100) ms -= 100;
+	// 	setInterval(function Milliseconds() {
+	// 		if (!isStoped) {
+	// 			ms++;
+	// 			if (ms >= 100) ms -= 100;
 
-				let msecond = ms < 10 ? "0" + ms : ms;
+	// 			let msecond = ms < 10 ? "0" + ms : ms;
 
-				$(".timer__milliseconds").html(msecond);
-			}
-		}, 10);
+	// 			$(".timer__milliseconds").html(msecond);
+	// 		}
+	// 	}, 10);
 
-		isStarted = true;
-	}
+	// 	isStarted = true;
+	// }
 
 	// Checkbox for timer
 	$(".timer__checkbox").on("click", function (e) {
